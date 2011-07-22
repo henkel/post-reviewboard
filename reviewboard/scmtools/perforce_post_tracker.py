d0386fc88d9055be9d98830034991e30609b53ee
Konstantin Welke <konstantin.welke@citrix.com>
2011-07-22 14:28:22 +0200

    get shelved changelists in get_missing_revisions

--- reviewboard/scmtools/perforce_post_tracker.py
+++ reviewboard/scmtools/perforce_post_tracker.py
@@ -14,10 +14,13 @@ from reviewboard.scmtools.post_utils import get_known_revisions, RepositoryRevis
 def extract_revision_user(line):
     # Try to extract revision info tuple (rev, user) from line
     # Revision info example: "116855 by henkel on 2011-03-24 11:30 AM"
-    words = line.split(' ',  4)
-                
+    words = line.split(' ',  5)
+
+    if len(words)>=5 and words[0] == 'Shelved:' and words[1].isdigit() and words[2] == 'by' and words[4] == 'on':
+        reuturn (words[1], words[3], line, True)
+        
     if len(words)>=4 and words[0].isdigit() and words[1] == 'by' and words[3] == 'on':
-        return (words[0], words[2])
+        return (words[0], words[2], line, False)
     
     return None
 
@@ -54,8 +57,9 @@ class PerforcePostCommitTrackerTool(PerforcePostCommitTool):
         # Revision exclusion predicate
         isExcluded = lambda rev : rev in known_revisions or rev in commits_to_be_ignored
         
+        #first compare by shelved or not (pos 3 in tuple), then by changenumber (pos 0 in tuple)
         sorted_revisions = sorted([ rev for rev in commits if not isExcluded(rev[0]) ], 
-                                  key=itemgetter(0), 
+                                  cmp=lambda x,y: cmp(int(x[0]),int(y[0])) if cmp(x[3],y[3]) == 0 else cmp(x[3],y[3]), 
                                   reverse=False) 
         return sorted_revisions
     
@@ -70,17 +74,22 @@ class PerforcePostCommitTrackerTool(PerforcePostCommitTool):
         
         try:
             day_plus_one = day + timedelta(days=1)
-            changes  = self.p4.run_changes('-l', '-s', 'submitted', '@' + day.strftime("%Y/%m/%d") + ',' + day_plus_one.strftime("%Y/%m/%d"))
+            #submitted changes
+            changes = self.p4.run_changes('-l', '-s', 'submitted', '@' + day.strftime("%Y/%m/%d") + ',' + day_plus_one.strftime("%Y/%m/%d"))
+            #extend by shelved changes. Note: those have a key 'shelved': ''
+            changes.extend(self.p4.run_changes('-l', '-s', 'shelved', '@' + day.strftime("%Y/%m/%d") + ',' + day_plus_one.strftime("%Y/%m/%d")))
 
             for changedesc in changes:
                 submit_date = datetime.fromtimestamp(int(changedesc['time']))        
                 date_str = submit_date.strftime("%Y-%m-%d")
                 
                 msg = changedesc['desc'].splitlines()[0].strip()
-                desc = 'on ' +date_str + ' : ' + msg
+                shelved = 'shelved' in changedesc
+                desc = 'Shelved: ' if shelved else '' + 'on ' +date_str + ' : ' + msg
                 log.append(( str(changedesc['change']), 
                              changedesc['user'], 
-                             desc))
+                             desc,
+                             shelved ))
         except Exception, e:
             raise SCMError('Error fetching revisions: ' +str(e))
         
